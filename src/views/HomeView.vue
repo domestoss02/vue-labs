b4a308ee5a70c57114ff4d3b764808fd127a91c7
unknown <kalybekajdarov265@gmail.com>
2022-12-15 11:09:12 +0600

    Clean project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="about">
+    <p>home</p>
+  </div>
 </template>
